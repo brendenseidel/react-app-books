be2d5087d540117b62b93b7e23c0841f7eb75cb1
Brenden Seidel <brenden.seidel@icloud.com>
2023-04-03 13:01:21 -0400

    complete api calls

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
 import BookCreate from "./components/BookCreate";
 import BookList from "./components/BookList";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
   const [books, setBooks] = useState([]);
+
+  const fetchBooks = async () => {
+    const response = await axios.get('http://localhost:3001/books')
+    setBooks(response.data)
+  }
+
+  useEffect(() => {
+    fetchBooks()
+  }, [])
 
   const createBook = async (title) => {
     const response = await axios.post('http://localhost:3001/books', { title })
     setBooks([...books, response.data]);
   }
 
-  const editBookById = (id, title) => {
+  const editBookById = async (id, title) => {
+    const response = await axios.put(`http://localhost:3001/books/${id}`, {
+      title
+    })
+
     const updatedBooks = books.map(book => {
       if (book.id === id) {
-        return {...book, title};
+        return {...book, ...response.data};
       }
       return book;
     })
@@ -22,7 +35,8 @@ function App() {
     setBooks(updatedBooks)
   }
 
-  const deleteBookById = (id) => {
+  const deleteBookById = async (id) => {
+    await axios.delete(`http://localhost:3001/books/${id}`)
     const updatedBooks = books.filter(book => {
       return book.id !== id;
     })
